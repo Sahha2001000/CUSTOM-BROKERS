370fa12c1185369e259c808d0b5c1062e31c64c1
Sahha2001000 <46458055+Sahha2001000@users.noreply.github.com>
2021-06-18 00:37:54 +0300

    commit 3: add block << contact-form >>, << footer>>(need ico viber, style edit), also add php script for request from site(with ajax), without adaptive(only PC)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
-$(document).ready(function () {
-    $('.btn__menu').click(function () {
-        $('.menu__list').slideToggle('');
-    });
-});
 
+// AJAX for contact-form
+(function ($) {
+    $(".form").submit(function (event) {
+        event.preventDefault();
+        // Сохраняем в переменную form id текущей формы, на которой сработало событие submit
+        let form = $('#' + $(this).attr('id'))[0];
+        // Сохраняем в переменную класс с параграфом для вывода сообщений
+        let message = $(this).find(".form__item");
+        let fd = new FormData(form);
+        $.ajax({
+            url: "/php/sendMsgTG.php",
+            type: "POST",
+            data: fd,
+            processData: false,
+            contentType: false,
+            success: function success() {
+                let respond = true
+                if (!respond) {
+                    alert('Щось пішло не так. Спробуйте відправити форму ще раз.');
+                } else {
+                    alert('Дякую! Ваша заявка прийнята. Ми зв\'яжемося з вами найближчим часом.');
+                }
+            },
+        });
+    });
+}(jQuery));
